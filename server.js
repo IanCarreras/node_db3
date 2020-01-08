16753274876fd299207f351a15d69b82decfdf46
Ian <mus5002@aol.com>
2020-01-07 21:31:36 -0500

    started scheme-model methods

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const logger = require('./middleware/logger')
+const notFound = require('./middleware/notFound')
+const error = require('./middleware/error')
 const SchemeRouter = require('./schemes/scheme-router.js');
+const server = express()
 
-const server = express();
+server.use(logger())
+server.use(express.json())
 
-server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+
+server.use(notFound())
+server.use(error())
 
 module.exports = server;
